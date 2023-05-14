3632f9ede63748573aeeb314255e0e29199bf1d0
QuocTuan <tuancq2011@gmail.com>
2023-05-14 20:14:10 +0700

    initial commit, path creator, editor utilities

---
+++ Assets/Scripts/Editor/PathEditor.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathCreator))]
+public class PathEditor : Editor
+{
+    PathCreator creator;
+    Path path;
+
+    private void OnSceneGUI()
+    {
+        Draw();
+        Input();
+    }
+
+    void Input()
+    {
+        //Input to add more anchor points
+        Event guiEvent = Event.current;
+        Vector2 mousePos = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin;
+
+        if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0 && guiEvent.shift)
+        {
+            Undo.RecordObject(creator, "Add segment");
+            path.AddSegment(mousePos);
+        }
+    }
+
+    void Draw()
+    {
+        //Draw bezier curve and line between anchor point and control point
+        for (int i = 0; i < path.NumSegments; i++)
+        {
+            Vector2[] points = path.GetPointsInSegment(i);
+            Handles.color = Color.black;
+            Handles.DrawLine(points[1], points[0]);
+            Handles.DrawLine(points[2], points[3]);
+            Handles.DrawBezier(points[0], points[3], points[1], points[2], Color.green, null, 2);
+        }
+
+
+        //Draw red handle which can be moved by hand
+        Handles.color = Color.red;
+        for (int i = 0; i < path.NumPoints; i++)
+        {
+            Vector2 newPos = Handles.FreeMoveHandle(path[i], Quaternion.identity, .1f, Vector2.zero, Handles.CylinderHandleCap);
+
+            // check if the red handle is being moved
+            if (path[i] != newPos)
+            {
+                Undo.RecordObject(creator, "Move point");
+                path.MovePoint(i, newPos);
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        creator = (PathCreator)target;
+
+        if (creator.path is null)
+        {
+            creator.CreatePath();
+        }
+
+        path = creator.path;
+    }
+}
